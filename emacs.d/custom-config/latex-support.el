3f37155a61be6614107c5cc047706962774604db
Daniel Grumberg <dany.grumberg@gmail.com>
2019-02-21 17:16:11 +0000

    Add support for building and adding macros in AucTeX

--- emacs.d/custom-config/latex-support.el
+++ emacs.d/custom-config/latex-support.el
@@ -7,14 +7,17 @@
     "." '(LaTeX-mark-environment :wk "mark-environment")
     "]" '(LaTeX-close-environment :wk "close-environment")
     "~" '(LaTeX-math-mode :wk "toggle-math-mode")
+    "b" '(TeX-command-run-all :wk "build")
     "B" '(LaTeX-find-matching-begin :wk "find-begin")
     "E" '(LaTeX-find-matching-begin :wk "find-end")
     "e" '(LaTeX-environment :wk "insert-environment")
     "i" '(LaTeX-insert-item :wk "insert-env-item")
+    "m" '(TeX-insert-macro :wk "insert-macro")
     "s" '(LaTeX-section :wk "insert-section"))
   :config
   (setq TeX-auto-save t
-        TeX-parse-self t)
+        TeX-parse-self t
+        TeX-PDF-mode t)
   (setq-default TeX-master nil))
 
 (provide 'dang/latex-support)
