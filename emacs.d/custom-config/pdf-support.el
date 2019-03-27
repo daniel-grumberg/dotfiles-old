b80ecd5b0178dc4199c74c5aebe1468550240e15
Daniel Grumberg <dany.grumberg@gmail.com>
2019-03-27 11:20:17 +0000

    Force PDF Tools to start in midnight mode.

--- emacs.d/custom-config/pdf-support.el
+++ emacs.d/custom-config/pdf-support.el
@@ -1,5 +1,6 @@
 (use-package pdf-tools
   :mode ("\\.pdf\\'" . pdf-view-mode)
+  :hook ((pdf-view-mode . pdf-view-midnight-minor-mode))
   :config
   (pdf-tools-install)
   (setq-default pdf-view-display-size 'fit-page))
