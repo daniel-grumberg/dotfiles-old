5d33cc09d66479667b342b4ef88f2343fb94e6e3
Daniel Grumberg <dany.grumberg@gmail.com>
2019-03-28 16:14:38 +0000

    Ensure pdfs that change on disk update

--- emacs.d/custom-config/pdf-support.el
+++ emacs.d/custom-config/pdf-support.el
@@ -1,8 +1,11 @@
 (use-package pdf-tools
   :mode ("\\.pdf\\'" . pdf-view-mode)
-  :hook ((pdf-view-mode . pdf-view-midnight-minor-mode))
+  :hook ((pdf-view-mode . pdf-view-midnight-minor-mode)
+         (pdf-view-mode . auto-revert-mode))
   :config
   (pdf-tools-install)
-  (setq-default pdf-view-display-size 'fit-page))
+  (setq-default pdf-view-display-size 'fit-page)
+  (setq auto-revert-interval 0.5)
+  (auto-revert-set-timer))
 
 (provide 'dang/pdf-support)
