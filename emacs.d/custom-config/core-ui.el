0751f5234efd7cb50d779dcad5b0b02f90ef0995
Daniel Grumberg <dany.grumberg@gmail.com>
2020-02-17 09:00:14 +0000

    Add hook that kills the compilation window after 2 seconds if it was successfull

--- emacs.d/custom-config/core-ui.el
+++ emacs.d/custom-config/core-ui.el
@@ -98,6 +98,16 @@
          ;; For some reason using fit-window-to-buffer on man pages makes the buffer tiny in width
          (side . right) (slot . 0))))
 
+(add-hook 'compilation-finish-functions
+          (lambda (buf str)
+            (if (null (string-match ".*exited abnormally.*" str))
+                ;;no errors, make the compilation window go away in a few seconds
+                (progn
+                  (run-at-time
+                   "2 sec" nil 'delete-windows-on
+                   (get-buffer-create "*compilation*"))
+                  (message "No Compilation Errors!")))))
+
 (dang/generate-override-keymap dang/leader/def "w" "windows")
 (dang/windows/def
   "b" 'balance-windows
